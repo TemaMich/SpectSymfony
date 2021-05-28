028a67962bfdba93c89b6c13cab4c87d3d674915
TemaMich <temamich18@gmail.com>
2021-05-28 19:54:30 +0300

    fourth commit

--- src/Controller/ListController.php
+++ src/Controller/ListController.php
@@ -7,20 +7,27 @@ use App\Entity\User;
 use App\Entity\Comment;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\Tools\Pagination\Paginator;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class ListController extends AbstractController
+class ListController extends Controller
 {
     /**
      * @Route("/list", name="list")
      */
-    public function index(EntityManagerInterface $em): Response
+    public function index(EntityManagerInterface $em, Request $request): Response
     {
         $listPosts = $em->getRepository(Post::class)->findBy([], ['created_at' => 'DESC']);
+        $posts = $this->get('knp_paginator')->paginate(
+            $listPosts,
+            $request->query->get('page', 1), 5
+        );
 
         return $this->render('list/index.html.twig', [
-            'listPosts' => $listPosts,
+            'listPosts' => $posts,
         ]);
     }
 }
